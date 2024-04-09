2caba7f4025d0dabedd83af8ead6f4017fa3b383
Dang_Duong_Huy <dhuy110399@gmail.com>
2024-04-09 15:29:48 +0700

    init
    Tải lên bảng SQL gốc từ trong database

---
+++ Cơ Sở dữ liệu quản lý danh sách sinh viên/SQL_goc.sql
@@ -0,0 +1,68 @@
+create database qldanhsachsinhvien
+on primary
+(name = qldanhsachsinhvien_data,
+filename = 'E:\SQL\qldanhsachsinhvien_data.mdf',
+size = 100mb,
+maxsize = 500mb,
+filegrowth = 10%)
+log on
+(name= qldanhsachsinhvien_log,
+filename = 'E:\SQL\qldanhsachsinhvien_log.ldf',
+size = 50mb,
+maxsize = unlimited,
+filegrowth = 5mb)
+
+use qldanhsachsinhvien
+go
+
+--tao bang du lieu
+--bang danh sach sinh vien
+create table SinhVien
+(
+	STT int identity(1,1),
+	iMaSV char(10) not null,
+	nTenSV nvarchar(100) not null,
+	iMaLop char(10) not null,
+	dNgaySinh datetime not null,
+	GioiTinh char(5) not null,
+	DiaChi nvarchar(100) not null,
+	SoDienThoai nvarchar(11) not null,
+	Email nvarchar(100) not null,
+	ChuyenNganh nvarchar(50) not null,
+	KhoaHoc nvarchar(10) not null
+)
+--bang danh sach lop
+create table Lop
+(
+	iMaLop char(10) not null,
+	nTenLop nvarchar(50) not null,
+	KhoaHoc nvarchar(10) not null, 
+	ChuyenNganh nvarchar(20) not null,
+	SiSo int,
+	tDanhSachSV char(10) not null,
+)
+--bang mon hoc
+create table MonHoc
+(
+	iMaMonHoc char(10) not null,
+	nTenMonHoc nvarchar(50) not null,
+	SoTinChi float,
+	MoTa nvarchar(100) not null,
+	iMaGiangVien char(10) not null,
+	HocKi int,
+	dNamHoc datetime,
+)
+--bang thoi khoa bieu
+create table ThoiKhoaBieu
+(
+	iMaThoiKhoaBieu char(10) not null,
+	iMaLop char(10) not null,
+	HocKi int,
+	dNamHoc datetime,
+	dThu nvarchar(10) not null,
+	sTietHoc char(10) not null,
+	iMaMonHoc char(10) not null,
+	nTenMonHoc nvarchar(50) not null,
+	iPhongHoc char(10) not null
+)
+go
